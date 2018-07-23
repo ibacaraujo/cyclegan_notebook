c50c9d9a895dbbfb0d0aa569ed6f76e6a0d6a6f3
ibaraujo <ibaraujo@deepone.vision.ime.usp.br>
2018-07-23 17:34:58 -0300

    Add cycle consistency loss

--- losses.py
+++ losses.py
@@ -1,7 +1,7 @@
 """
 Losses module for CycleGAN
 
-This module contains the implementation of the losses used in CycleGAN work.
+This module contains the implementation of the losses used in CycleGAN framework.
 
 The losses are divided into two categories: 
   - Building block losses;
@@ -22,9 +22,20 @@ import numpy as np
 ## Building block losses ##
 
 # L1 Loss
-def l1_loss(y, y_hat):
-  return np.sum(np.abs(y - y_hat))
+def l1_loss(x, y):
+  return np.sum(np.abs(x - y))
 
 # L2 Loss
-def l2_loss(y, y_hat):
-  return np.sum(np.square(y - y_hat))
+def l2_loss(x, y):
+  return np.sum(np.square(x - y))
+
+## Adversarial Losses ##
+
+# TODO
+
+## Cycle Consistency Loss ##
+def cycle_consistency_loss(G, F, x, y):
+  forward_cycle_consistency = np.mean(l1_loss(F(G(x), x)))
+  backward_cycle_consistency = np.mean(l1_loss(G(F(y), y)))
+  lambda_value = 10.0
+  return lambda_value * (forward_cycle_consistency + backward_cycle_consistency)
