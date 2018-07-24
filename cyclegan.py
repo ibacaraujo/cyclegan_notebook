28da8f078a8f015ca3576fa354292af24450bfd8
ibaraujo <ibaraujo@deepone.vision.ime.usp.br>
2018-07-24 16:48:38 -0300

    Update cyclegan.py

--- cyclegan.py
+++ cyclegan.py
@@ -3,10 +3,5 @@ CycleGAN
 """
 
 import numpy as np
-from losses import l1_loss, l2_loss
-
-y = np.array([[1, 1, 1, 1], [1, 1, 1, 1]])
-y_hat = np.array([[1, 1, 1, 1], [1, 1, 1, 1]])
-
-print(l1_loss(y, y_hat))
-print(l2_loss(y, y_hat))
+import tensorflow as tf
+import tensorflow.contrib.layers as tfl
