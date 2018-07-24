5626ab9b76064b559784f39866251a760de68406
ibaraujo <ibaraujo@deepone.vision.ime.usp.br>
2018-07-24 16:59:37 -0300

    Add encoder

--- cyclegan.py
+++ cyclegan.py
@@ -2,6 +2,50 @@
 CycleGAN
 """
 
+# Load libraries
+
 import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.layers as tfl
+
+# The Generator
+
+def encoder(x, n_filters=32, k_size=3, normalizer_fn=instance_norm, 
+        activation_fn=lrelu, scope=None, reuse=None):
+    with tf.variable_scope(scope or 'encoder', reuse=reuse):
+        h = tf.pad(x, [[0, 0], [k_size, k_size], [k_size, k_size], [0, 0]],
+                "REFLECT")
+        h = tfl.conv2d(
+                inputs=h,
+                num_outputs=n_filters,
+                kernel_size=7,
+                stride=1,
+                padding='VALID',
+                weights_initializer=tf.truncated_normal_initializer(stddev=0.02),
+                biases_initializer=None,
+                normalizer_fn=normalizer_fn,
+                activation_fn=activation_fn,
+                scope='1',
+                reuse=reuse)
+        h = tfl.conv2d(
+                inputs=h,
+                num_outputs=n_filters*2,
+                kernel_size=k_size,
+                stride=2,
+                weights_initializer=tf.truncated_normal_initializer(stddev=0.02),
+                biases_initializer=None,
+                normalizer_fn=normalizer_fn,
+                activation_fn=activation_fn,
+                scope='2',
+                reuse=reuse)
+        h = tfl.conv2d(inputs=h,
+                num_outputs=n_filters*4,
+                kernel_size=k_size,
+                stride=2,
+                weights_initializer=tf.truncated_normal_initializer(stddev=0.02),
+                biases_initializer=None,
+                normalizer_fn=normalizer_fn,
+                activation_fn=activation_fn,
+                scope='3',
+                reuse=reuse)
+        return h
