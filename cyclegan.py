929eb719c0bd91f855137393924734dfc4f90783
ibaraujo <ibaraujo@deepone.vision.ime.usp.br>
2018-07-25 17:18:42 -0300

    Add residual block

--- cyclegan.py
+++ cyclegan.py
@@ -50,4 +50,34 @@ def encoder(x, n_filters=32, k_size=3, normalizer_fn=instance_norm,
                 reuse=reuse)
         return h
 
+def residual_block(x, n_channels=128, normalizer_fn=instance_norm,
+        activation_fn=lrelu, kernel_size=3, scope=None, reuse=None):
+    with tf.variable_scope(scope or 'residual', reuse=reuse):
+        h = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], "REFLECT")
+        h = tfl.conv2d(
+                inputs=h,
+                num_outputs=n_channels,
+                kernel_size=kernel_size,
+                weights_initializer=tf.truncated_normal_initializer(stddev=0.02),
+                biases_initializer=None,
+                normalizer_fn=normalizer_fn,
+                padding='VALID',
+                activation_fn=activation_fn,
+                scope='1',
+                reuse=reuse)
+        h = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], "REFLECT")
+        h = tfl.conv2d(
+                inputs=h,
+                num_outputs=n_channels,
+                kernel_size=kernel_size,
+                weights_initializer=tf.truncated_normal_initializer(stddev=0.02),
+                biases_initializer=None,
+                normalizer_fn=normalizer_fn,
+                padding='VALID',
+                activation_fn=None,
+                scope='2',
+                reuse=reuse)
+        h = tf.add(x, h)
+    return h
     # TODO: add lrelu, instance_norm, residual_blocks, transform and decoder
+
